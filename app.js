8187d5a2d8851d9edc26ecdeea339255b0371509
Khalid <Khalid.Ali@consultant.com>
2015-05-15 09:39:09 -0400

    refactor code for thee game network

--- app.js
+++ app.js
@@ -1,32 +1,120 @@
+/*
+
+Server Side JavaScript for TicTacToe
+Author: Khalid Omar Ali
+
+CLIENT/SERVER MODEL:
+Each player is a client and all clients communicate with server
+
+PLAYER JOINS: 
+Each player exists as an object on the server
+When a player logs on, they should be updated with the status
+of each player on the server
+Each player must joing a room/lobby to ensure they start from
+the same initial game state
+
+GAME STATE: 
+The game state exists on the server and mirrored on clients
+When a player clicks, the client emits X or O to the server
+Server update state of each character in the 'world' and 
+replies back with a packet containing the state of my character
+and other player
+Clients simply incorporate the updates from server
+
+RESOURCE: 
+Glenn Fiedler's What Every Programmer Needs to know
+about Game Networking  
+
+*/
+
+'use strict';
+
 // require express
 var express = require("express");
 // declare a new instance of express
 var app = express();
-// app supplied as arg to HTTP server
+// app supplied as argument to HTTP server
 var server = require("http").Server(app);
 // require path to handle file paths
 var path = require("path");
-// serve static files via express
-app.use(express.static(path.join(__dirname, "public")));
-var publicDir = "./public/";
-// define route
-app.get("/", function(req, res) {
-	res.sendFile(publicDir + "index.html");
-});
 // require socket.io and pass server obj
 var io = require("socket.io")(server);
-// listen to port 3000 or whatever is in process.env.port 
-var port = process.env.port || 3000;
-server.on("listening", function() {
-    console.log("OK, the server is listening ");
+// require randomString
+// var randomString = require("randomstring");
+// curent tcp port
+var port;
+
+// add static middleware serving files under public folder
+app.use(express.static(path.join(__dirname, "public")));
+// routes
+app.get("/", function(req, res) {
+	var gameId = randomstring.generate(5);
+	res.redirect("/game/" + gameId);
 });
+app.get("/game/:id", function(req, res) {
+	var publicDir = "./public/";
+	res.sendFile(publicDir + "indext.html");
+});
+// listen for connection event to socket.io
+io.on("connection", function(client) {
+	console.log("socket.io connection established");
+	// define states
+	var players = [],
+		roomId, totalMoves, turns, player1Id, player2Id, startGame, drawGame;
+	// event handler for when each client joins 
+	client.on("join", function(name, callback) {
+		// check if name isn't in players array
+		if (players.indexOf(name) === -1) {
+			// callback returns true to client o indicate player name is free
+			callback(true);
+			// put name as property of client's socket object
+			client.username = name;
+			// push name into player's array
+			players.push(client.username);
+			// emit player name to all (emit should be to the second player only!)
+			io.emit("players", players);
+		}
+	});
+	// room is an arbitrary channel that clients joint or leave
+	client.on("room", function(room) {
+		var gameRoom, clientsNum;
+		// join arbitrary room
+		client.join(room);
+		roomId = room;
+		console.log("client connected to room " + roomId);
+		// an aray of all clients connected on socket
+		gameRoom = io.sockets.clients(roomId);
+		// clientsNum is the length of gameRoom
+		clientsNum = gameRoom.length;
+		// identify players
+		if (clientsNum === 1) {
+			player1Id = gameRoom[0].id;
+			io.sockets.socket(player1Id).emit("player", 1);
+		} else if (clientsNum === 2) {
+			player2Id = gameRoom[1].id;
+			io.sockets.socket(player2Id).emit("player", 2);
+		} else {
+			return; // don't do anything
+		}
+	}); * /
+	// listen for disconnect event and remove player name
+	client.on("disconnect", function() {
+		var startIndex = players.indexOf(client.username);
+		if (client.username) {
+			// remove from client.username from player's array
+			players.splice(startIndex, 1);
+			io.emit("disconnected", players);
+		} else {
+			return;
+		}
+	});
+});
+// listening event handler for server
+server.on("listening", function() {
+	console.log("OK, the server is listening ");
+});
+port = process.env.port || 3000;
+// listen to whatever is in process.env.port or port 3000 
 server.listen(port, function() {
 	console.log("listening on port " + port);
-});
-io.on("connection", function (client) {
-	console.log("socket.io connection established");
-	// listen for events emitted from client
-	socket.on("disconnect", function() {
-		io.emit("user disconnected");
-	});
 });
--- public/client.js
+++ public/client.js
@@ -1,168 +1,89 @@
-// invoke socket.io
-var socket = io("http://localhost.com");
-// get users to join the game
+/*
 
-// bind helper function
-if (!Function.prototype.bind) { // credit to Crockford for this bind function  
-    Function.prototype.bind = function(oThis) {
-        if (typeof this !== "function") {
-            // closest thing possible to the ECMAScript 5 internal IsCallable function
-            throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
-        }
-        var aArgs = Array.prototype.slice.call(arguments, 1),
-            fToBind = this,
-            fNOP = function() {},
-            fBound = function() {
-                return fToBind.apply(this instanceof fNOP && oThis ? this : oThis, aArgs.concat(Array.prototype.slice.call(arguments)));
-            };
-        fNOP.prototype = this.prototype;
-        fBound.prototype = new fNOP();
-        return fBound;
-    };
-}
+Client-Side JavaScript for Multi-PLayer TicTacToe
+Author: Khalid Omar Ali
 
-var TicTacToe = function() { // current function constructor
-    this.init();
-};
+*/
 
-TicTacToe.prototype = { // gives instances of TicTacToe the below methods and values
-    boardElem: null,
-    resultElem: null,
-    startElem: null,
-    statusElem: null,
-    xTurn: true,
-    gameOver: false,
-    moves: 0,
-
-    init: function() {
-        // bind UI
-        this.startElem = document.getElementById("start-button");
-        this.boardElem = document.getElementById("game-board");
-        this.resultElem = document.getElementById("results");
-        this.statusElem = document.getElementById("status");
-        // current binded events on starbutton and game board
-        this.startElem.onclick = function() {
-            this.startGame();
-        }.bind(this);
-        this.boardElem.onclick = function(e) {
-            e = e || event;
-            var source = e.boardElem || e.target;
-            var id = source.getAttribute("id"); // get all btns 1...9 
-            this.getSquareValues(id); // pass the id of the buttons clicked
-        }.bind(this);
-    },
-
-    startGame: function(square) {
-        var sqrArr = document.getElementsByTagName("input");
-        for (var i = 0; i < sqrArr.length; i += 1) {
-            sqrArr[i].value = "";
-        }
-        this.resultElem.innerHTML = "";
-        this.getStatus();
-        this.moves = 0;
-        this.gameOver = false;
-        this.getSquareValues();
-    },
-
-    getStatus: function() {
-        return this.statusElem.innerHTML = "It is the turn of " + (this.xTurn ? "X" : "O");
-    },
-
-    increment: function() {
-        return this.moves += 1;
-    },
-
-    getSquareValues: function(id) {
-        var squareElem = document.getElementById(id);
-        // stop clicking a square with a value
-        if (squareElem && squareElem.value) {
-            return;
-        }
-        // play turn if square is free and game is not over
-        if (squareElem && squareElem.value == "" && this.gameOver !== true) { // stops a square being selected more than once
-            if (this.xTurn) {
-                this.increment();
-                squareElem.value = "X";
-                this.xTurn = false;
-                this.getStatus();
-            } else {
-                this.increment();
-                squareElem.value = "O";
-                this.xTurn = true;
-                this.getStatus();
+(function() {
+    'use strict';
+    // create a new socket instance 
+    var socket = io.connect("http://localhost:3000");
+    // get users to join the game by first getting elements
+    var userWrapper = document.getElementById("user-wrapper"),
+        userForm = document.getElementById("user-form"),
+        userInput = document.getElementById("user-input"),
+        userErr = document.getElementById("user-err"),
+        userElem = document.getElementById("user-list"),
+        gameWrapper = document.getElementById("game-wrapper");
+    // once you connect to socket.io, emit room event and a room (arbitrary channel);
+    socket.on("connect", function(room) {
+        socket.emit("room", room);
+    });
+    // credit to Crockford for this bind function
+    if (!Function.prototype.bind) {
+        Function.prototype.bind = function(oThis) {
+            if (typeof this !== "function") {
+                // closest thing possible to the ECMAScript 5 internal IsCallable function
+                throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
             }
-        } else {
-            return;
-        }
-        this.moves === 9 ? this.gameOver = true : this.gameOver = false;
-        this.checkForWinner();
-    },
-
-    checkForWinner: function() { // check all 8 winning combinations 
-        var arr = []; // declare an array that holds all button values
-        // put all values of all 9 squares into the array
-        arr[1] = document.getElementById("btn1").value;
-        arr[2] = document.getElementById("btn2").value;
-        arr[3] = document.getElementById("btn3").value;
-        arr[4] = document.getElementById("btn4").value;
-        arr[5] = document.getElementById("btn5").value;
-        arr[6] = document.getElementById("btn6").value;
-        arr[7] = document.getElementById("btn7").value;
-        arr[8] = document.getElementById("btn8").value;
-        arr[9] = document.getElementById("btn9").value;
-        // check if there are three Xs or three Os in a row  
-        if (arr[1] === arr[2] && arr[2] === arr[3] && arr[1] !== "") {
-            this.resultElem.innerHTML = arr[1] + ' wins';
-            this.gameOver = true;
-            this.statusElem.innerHTML = "";
-            return;
-        } else if (arr[4] === arr[5] && arr[5] === arr[6] && arr[4] !== "") {
-            this.resultElem.innerHTML = arr[4] + ' wins';
-            this.gameOver = true;
-            this.statusElem.innerHTML = "";
-            return;
-        } else if (arr[7] === arr[8] && arr[8] == arr[9] && arr[7] !== "") {
-            this.resultElem.innerHTML = arr[7] + ' wins';
-            this.gameOver = true;
-            this.statusElem.innerHTML = "";
-            return;
-        }
-        // check if there are three Xs or three Os in a column   
-        else if (arr[1] === arr[4] && arr[4] === arr[7] && arr[1] !== "") {
-            this.resultElem.innerHTML = arr[1] + ' wins';
-            this.gameOver = true;
-            this.statusElem.innerHTML = "";
-            return;
-        } else if (arr[2] === arr[5] && arr[5] === arr[8] && arr[2] !== "") {
-            this.resultElem.innerHTML = arr[2] + ' wins';
-            this.gameOver = true;
-            this.statusElem.innerHTML = "";
-            return;
-        } else if (arr[3] === arr[6] && arr[6] == arr[9] && arr[3] !== "") {
-            this.resultElem.innerHTML = arr[3] + ' wins';
-            this.gameOver = true;
-            this.statusElem.innerHTML = "";
-            return;
-        }
-        // check if there are three Xs or three Os in a diagonal
-        else if (arr[7] === arr[5] && arr[5] === arr[3] && arr[7] !== "") {
-            this.resultElem.innerHTML = arr[7] + ' wins';
-            this.gameOver = true;
-            this.statusElem.innerHTML = "";
-            return;
-        } else if (arr[1] === arr[5] && arr[5] === arr[9] && arr[1] !== "") {
-            this.resultElem.innerHTML = arr[1] + ' wins';
-            this.gameOver = true;
-            this.statusElem.innerHTML = "";
-            return;
-        } else if (this.moves === 9) {
-            this.resultElem.innerHTML = "draw";
-            this.gameOver = true;
-            return;
-        } else {
-            this.resultElem.innerHTML = "";
-            return;
-        }
+            var aArgs = Array.prototype.slice.call(arguments, 1),
+                fToBind = this,
+                fNOP = function() {},
+                fBound = function() {
+                    return fToBind.apply(this instanceof fNOP && oThis ? this : oThis, aArgs.concat(Array.prototype.slice.call(arguments)));
+                };
+            fNOP.prototype = this.prototype;
+            fBound.prototype = new fNOP();
+            return fBound;
+        };
     }
-};
-var playGame = new TicTacToe(); // new object which inherits from TicTacToe
+    // add players to game
+    userForm.addEventListener("submit", function(e) {
+        // work around for IE
+        e = e || event;
+        // prevent default hebaviour of form
+        e.preventDefault();
+        socket.emit("join", userInput.value, function(isNameAvailable) {
+            // if callback from server is true 
+            if (isNameAvailable) {
+                // hide user input area
+                userWrapper.style.display = "none";
+                // start game
+                gameWrapper.style.display = "block";
+                // if name is already taken
+            } else {
+                // handle error
+                userErr.innerHTML = "Name not available, please select another name!";
+            }
+            // clear user input
+            userInput.value = "";
+        });
+    });
+
+    function addPlayers(data) {
+            var i, str = "",
+                len = data.length;
+            for (i = 0; i < len; i += 1) {
+                str += data[i] + "</br>";
+            }
+            userElem.innerHTML = str;
+        }
+    // listen for player names and display
+    socket.on("players", function(data) {
+        addPlayers(data);
+    });
+    /*
+        // identify player
+        socket.on("player", function(player) {
+            if (player === 1 && player === 2) {
+                console.log("game started, this is player " + player + ".");
+            }
+        });
+    */
+    // remove players upon disconnection from socket
+    socket.on("disconnected", function(data) {
+        addPlayers(data);
+    });
+
+})();

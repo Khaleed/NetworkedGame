a3f9d97c157cd30243eafa6bc9fcb4bd2eefd40a
Khalid <Khalid.Ali@consultant.com>
2015-05-21 10:04:40 -0400

    refactor code again to shorten code and simplify game logic

--- app.js
+++ app.js
@@ -1,34 +1,24 @@
 /*
-
 Server Side JavaScript for TicTacToe
 Author: Khalid Omar Ali
-
 CLIENT/SERVER MODEL:
-
 Each player is a client and all clients communicate with server
-
 PLAYER JOINS: 
-
 Each player exists as an object on the server
 When a player logs on, they should be updated with the status
 of each player on the server
 Each player must join a room/lobby to ensure they start from
 the same initial game state
-
 GAME STATE: 
-
 The game state exists on the server and mirrored on clients
 When a player clicks, the client emits X or O to the server
 Server updates state of each character in the 'world' and 
 replies back with a packet containing the state of the 
 character of player
 Clients simply incorporate the updates from server
-
 RESOURCE: 
-
 Glenn Fiedler's What Every Programmer Needs to know
 about Game Networking  
-
 */
 
 'use strict';
@@ -50,48 +40,6 @@ app.use(express.static(path.join(__dirname, "public")));
 // listen for connection event to socket.io
 io.on("connection", function(client) {
 	console.log("socket.io connection established");
-	// define states
-	var players = [];
-	// listen for when each client adds a user and emit the username to all players
-	client.on("addUser", function(name, callback) {
-		// check if name isn't in players array
-		if (players.indexOf(name) === -1) {
-			// callback returns true to client o indicate player name is free
-			callback(true);
-			// put name as property of client's socket object
-			// name is available both on server and client
-			client.username = name;
-			// push name into player's array
-			players.push(client.username);
-			// emit player name 
-			io.emit("players", players);
-		} else {
-			return callback(false);
-		}
-	});
-	// startGame
-	client.on("start game", function(data) {
-		// check if there are two players
-		if(players.length === 2) {
-			data();
-		} else {
-			return;
-		}
-	});
-	
-	// listen for disconnect event and remove player name
-	client.on("disconnect", function() {
-		var startIndex = players.indexOf(client.username);
-		if (client.username) {
-			// remove from client.username from player's array
-			players.splice(startIndex, 1);
-			// broadcast the clients that left and send data to client
-			io.emit("disconnected", players);
-			console.log(client.username + " left the game");
-		} else {
-			return;
-		}
-	});
 });
 // listening event handler for server
 server.on("listening", function() {
@@ -102,4 +50,3 @@ port = process.env.port || 3000;
 server.listen(port, function() {
 	console.log("listening on port " + port);
 });
-
--- public/client.js
+++ public/client.js
@@ -6,183 +6,156 @@ Author: Khalid Omar Ali
 */
 
 (function() {
-        'use strict';
-        // create a new socket instance 
-        var socket = io.connect("http://localhost:3000"),
-            // set game elements to null
-            boardElem = null,
-            resultElem = null,
-            startElem = null,
-            statusElem = null,
-            xTurn = true,
-            gameOver = false,
-            moves = 0,
-            // username elements
-            userWrapper = document.getElementById("user-wrapper"),
-            userForm = document.getElementById("user-form"),
-            userInput = document.getElementById("user-input"),
-            userErr = document.getElementById("user-err"),
-            userElem = document.getElementById("user-list"),
-            gameWrapper = document.getElementById("game-wrapper"),
-            // board elements
-            startElem = document.getElementById("start-button"),
-            boardElem = document.getElementById("game-board"),
-            squares = document.getElementsByTagName("input"),
-            resultElem = document.getElementById("results"),
-            statusElem = document.getElementById("status"),
-            boardArr = [],
-            winCombo = [
-                [0, 1, 2],
-                [3, 4, 5],
-                [6, 7, 8],
-                [0, 3, 6],
-                [1, 4, 7],
-                [2, 5, 8],
-                [0, 4, 8],
-                [2, 4, 6]
-            ];
-        // add players to game
-        userForm.addEventListener("submit", function(e) {
-            // work around for IE
-            e = e || event;
-            // prevent default hebaviour of form
-            e.preventDefault();
-            socket.emit("addUser", userInput.value, function(isNameAvailable) {
-                // if callback from server is true 
-                if (isNameAvailable) {
-                    // hide user input area
-                    userWrapper.style.display = "none";
-                    // start game
-                    gameWrapper.style.display = "block";
+
+    'use strict';
+
+    // var socket = io.connect("http://localhost:3000");
+    if (!Function.prototype.bind) { // credit to Crockford for this bind function  
+        Function.prototype.bind = function(oThis) {
+            if (typeof this !== "function") {
+                // closest thing possible to the ECMAScript 5 internal IsCallable function
+                throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+            }
+            var aArgs = Array.prototype.slice.call(arguments, 1),
+                fToBind = this,
+                fNOP = function() {},
+                fBound = function() {
+                    return fToBind.apply(this instanceof fNOP && oThis ? this : oThis, aArgs.concat(Array.prototype.slice.call(arguments)));
+                };
+            fNOP.prototype = this.prototype;
+            fBound.prototype = new fNOP();
+            return fBound;
+        };
+    }
+
+    var NodeTacToe = function() { // current function constructor 
+        this.init(); // constructor invocation method - this bound to the new object 
+    };
+    // this is the prototype object associated with the above function constructor 
+    NodeTacToe.prototype = {
+        // set initial game state
+        boardElem: null,
+        resetElem: null,
+        resultElem: null,
+        statusElem: null,
+        moves: 0,
+        xTurn: true,
+        gameOver: false,
+        boardArr: [],
+        winCombo: [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ],
+
+        resetGame: function() {
+            var elem, i, j;
+            for (i = 0; i <= 8; i += 1) {
+                this.boardArr[i] = null; // set all values of board array to null
+            }
+            for (j = 0; j <= 8; j += 1) {
+                this.squares[j].value = ""; // clear the board UI
+            }
+            this.moves = 0; // current move incrementor
+            this.gameOver = false;
+            this.xTurn = true; // X is first player
+            this.statusElem.innerHTML = "";
+        },
+
+        init: function() {
+            // get elements
+            var src;
+            this.resetElem = document.getElementById("start-button");
+            this.boardElem = document.getElementById("game-board");
+            this.resultElem = document.getElementById("results");
+            this.statusElem = document.getElementById("status");
+            this.squares = document.getElementsByTagName("input");
+            this.resetGame(); // reset game when game is first initialised
+            // bind main event handlers to reset game amd click board        
+            this.resetElem.onclick = function() {
+                this.resetGame(); // clear board once reset button is clicked
+            }.bind(this); // inner function's this now bounded to the this variable of the outer function 
+            // use bind method instead of writing var that = this
+            this.boardElem.onclick = function(e) { // e is the event object passed as an argument
+                e = e || event; // event sometimes available through the global variable event for IE
+                src = e.boardElem || e.target; // event target is the object which the event is associated with - cross browser issue
+                this.renderMove(this.boardArr, { // pass an object containing the element and its data-position to renderMove method
+                    position: src.getAttribute("data-position"),
+                    elem: src
+                });
+            }.bind(this);
+        },
+
+        isSquareAvailable: function(board, position) {
+            return board[position] === null; // returns true if space is available
+        },
+
+        checkForWinningMove: function() { // check 3 rows, 3 columns and 2 diagonals using winCombo array that holds all winning combinations of the game
+            var i;
+            for (i = 0; i < this.winCombo.length; i += 1) {
+                if (this.boardArr[this.winCombo[i][0]] === this.boardArr[this.winCombo[i][1]] && this.boardArr[this.winCombo[i][1]] === 
+                    this.boardArr[this.winCombo[i][2]] && this.boardArr[this.winCombo[i][1]] !== null) {
+                    return true;
+                } 
+            }
+        },
+
+        validMoves: function(board) {
+            var i, totalMoves = [];
+            board = this.boardArr;
+            for (i = 0; i < 10; i += 1) {
+                if (board[i] !== null) {
+                    totalMoves.push[i];
+                }
+                return totalMoves;
+            }
+        },
+
+        drawn: function() {
+            if (this.validMoves() === 9) {
+                alert("Draw game!");
+                this.resetGame();
+            }
+        },
+
+        won: function(result) {
+            result = this.checkForWinningMove();
+            if (result === true) { // if there is a winner
+                if (this.xTurn === false) { // decide if X won or else O won
+                    alert("X wins!");
+                    this.resetGame();
                 } else {
-                    // handle error
-                    userErr.innerHTML = "Name not available, please select another name!";
-                }
-                // clear user input
-                userInput.value = "";
-            });
-        });
-        // add users
-        function addPlayers(data) {
-                var i, str = "",
-                    len = data.length;
-                for (i = 0; i < len; i += 1) {
-                    str += data[i] + "</br>";
-                }
-                userElem.innerHTML += str;
-            }
-            // listen for player names and display
-        socket.on("players", function(data) {
-            addPlayers(data);
-        });
-        // game board event handler
-        boardElem.addEventListener("click", function(e) {
-            var i, pos, src;
-            e = e || event;
-            src = e.boardElem || e.target;
-            // get position of the square being clicked
-            pos = src.getAttribute("data-position");
-            // pass position to updateGame method
-            updateGame(boardArr, {
-                pos: src.getAttribute("data-position"),
-                elem: src
-            });
-            // starGame
-            function startGame() {
-                    for (i = 0; i < squares.length; i += 1) {
-                        squares[i].value = "";
-                    }
-                    moves = 0;
-                    gameOver = false;
-                    getSquareValues();
-                }
-                // check if square is available
-            function isSquareAvailable(board, position) {
-                    return board[position] === null;
-                }
-                // get total validMoves
-            function validMoves(board) {
-                    board = boardArr;
-                    var totalMoves = [];
-                    for (var i = 1; i < 10; i += 1) {
-                        if (board[i] === null) {
-                            totalMoves.push[i];
-                        }
-                        return totalMoves;
-                    }
-                }
-                // check if the game is drawn
-            function drawGame() {
-                    if (totalMoves === 9) {
-                        alert("Draw Game!");
-                        gameOver = true;
-                        startGame();
-                    }
-                }
-                // check if there is a winner
-            function checkForWin() {
-                    // check 8 winning combo: 3 rows, 3 columns, and 2 diagonals
-                    var i;
-                    for (i = 0; i < winCombo.length; i += 1) {
-                        if (boardArr[winCombo[i][0]] === boardArr[winCombo[i][1]] && boardArr[winCombo[i][1]] === boardArr[winCombo[i][2]] &&
-                            boardArr[winCombo[i][1]] !== null) {
-                            return true;
-                        }
-                    }
-                }
-            // update game status
-            function getStatus() {
-                    xTurn = true ? statusElem.innerHTML = "It's the turn of X" : statusElem.innerHTML = "It's the turn of O";
-                }
-            // check who won
-            function won() {
-                var result = checkForWin();
-                if (result === true) {
-                    if (xTurn === false) {
-                        alert("X wins");
-                        gameOver = true;
-                        starGame();
-                    } else {
-                        alert("O wins");
-                        gameOver = true;
-                        starGame();
-                    }
+                    alert("O wins!");
+                    this.resetGame();
                 }
             }
-            // is it draw or win
-            function decideGame() {
-                    return draGame() || won();
-                }
-            // update game and emit
-            function updateGame(board, square) {
-                    var squareElem = square.elem,
-                        squarePos = square.pos;
-                    if (isSquareAvailable(board, squarePos)) {
-                        console.log("is there a free square", board[squarePos] === null);
-                        if (xTurn === true && this.gameOver !== true) {
-                            moves += 1;
-                            boardArr[squarePos] = "X";
-                            xTurn = false;
-                            getStatus();
-                            decideGame();
-                        } else {
-                            moves += 1;
-                            boardArr[squarePos] = "O";
-                            xTurn = true;
-                            getStatus();
-                            decideGame();
-                        }
-                    }
-                    else {
-                        statusElem.innerHTML = "That square is taken!";
-                    }
-                }
-            // start element event handler
-            startElem.addEventListener("click", function() {
-                socket.emit("start game", startGame());
-            });
-            // remove players upon disconnection from socket
-            socket.on("disconnected", function(data) {
-                addPlayers(data);
-            });
-        })();
+        },
+
+        isGameOver: function() {
+            return this.won() || this.drawn(); // decide the game's outcome
+        },
+
+        renderMove: function(board, square, squareElem, squarePos) { // player clicks the squares
+            squareElem = square.elem;
+            squarePos = square.position;
+            // stop selecting a square that's already taken
+            if (squareElem.value !== "") {
+                return;
+            }
+            if (this.isSquareAvailable(board, squarePos) && this.gameOver !== true) { // check board array is null and game isn't over
+                this.moves += 1; // increment game move
+                board[squarePos] = this.xTurn ? "X" : "O";
+                this.xTurn = !this.xTurn;
+                squareElem.value = board[squarePos]; // udpate UI
+                this.statusElem.innerHTML = "It's the turn of " + (this.xTurn ? "X" : "O");
+            }
+            this.isGameOver();
+        }
+    };
+    var playNodeTacToe = new NodeTacToe(); // create a new instance of the GameBoard
+})();

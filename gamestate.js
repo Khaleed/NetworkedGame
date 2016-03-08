39ed2d34939f9523da1987d2e88d5455218bea46
Khalid Omar Ali <khalid.ali@consultant.com>
2016-03-08 03:50:54 +0000

    correct spelling

--- gamestate.js
+++ gamestate.js
@@ -25,51 +25,51 @@ function GameState() {
 		[2, 4, 6]
 	];
 
-	var playerQue = [];
+  var playerQueue = [];
 
 	self.resetBoard = function() {
 		self.board = new Array(9);
 	};
 
-	self.getPlayerNoFromQue = function(id) {
+  self.getPlayerNoFromQueue = function(id) {
 		if (typeof id === "undefined") {
-			console.log("id undefined in getPlayerNoFromQue fn");
+      console.log("id undefined in getPlayerNoFromQueue fn");
 			return false;
 		}
-		var index = playerQue.indexOf(id);
+    var index = playerQueue.indexOf(id);
 		if (index === -1) {
-			console.log("negative index in getPlayerNoFromQue fn");
+      console.log("negative index in getPlayerNoFromQueue fn");
 			return false;
 		}
 		return index;
 	};
 
-	self.addPlayerToQue = function(id) {
+  self.addPlayerToQueue = function(id) {
 		// defensive check
 		if (typeof id === "undefined") {
-			console.log("id undefined in addPlayerToQue fn");
+      console.log("id undefined in addPlayerToQueue fn");
 			return false;
 		}
 		// fail quick test
-		if (playerQue.length === self.MAX_PLAYERS) {
-			console.log("playerQue length equal to MAX_PLAYERS in addPlayerToQue fn");
+    if (playerQueue.length === self.MAX_PLAYERS) {
+      console.log("playerQueue length equal to MAX_PLAYERS in addPlayerToQueue fn");
 			return false;
 		}
-		playerQue.push(id);
+    playerQueue.push(id);
 		return true;
 	};
 
-	self.deletePlayerFromQue = function(id) {
+  self.deletePlayerFromQueue = function(id) {
 		if (typeof id === "undefined") {
-			console.log("id undefined in deletePlayerFromQue fn");
+      console.log("id undefined in deletePlayerFromQueue fn");
 			return false;
 		}
-		if (playerQue.length === 0) {
-			console.log("playerQue length equal to 0 in playerQue fn");
+    if (playerQueue.length === 0) {
+      console.log("playerQueue length equal to 0 in playerQueue fn");
 			return false;
 		}
-		var index = playerQue.indexOf(id);
-		playerQue.splice(index, 1);
+    var index = playerQueue.indexOf(id);
+    playerQueue.splice(index, 1);
 		return true;
 	};
 
